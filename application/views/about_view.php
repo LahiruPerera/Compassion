39cf4801d63b42180cc637f38f0214b9fce7df5f
geethmi <geethmi93@gmail.com>
2016-12-06 21:57:20 -0800

    Create about_view.php

---
+++ application/views/about_view.php
@@ -0,0 +1,332 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Compassion</title>
+<link href="<?php echo base_url(); ?>compassion/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="<?php echo base_url(); ?>compassion/js/jquery-1.11.0.min.js"></script>
+<!-- Custom Theme files -->
+<link href="<?php echo base_url(); ?>compassion/css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Donate Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Karla:400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Archivo+Narrow:400,700' rel='stylesheet' type='text/css'>
+<!--google fonts-->
+</head>
+<body>
+<!--banner start here-->
+<div class="banner1">
+	<div class="header1">
+	   <div class="container">
+		 <div class="header-main">
+		   <div class="logo">
+			 <h1><a href="index.html">Compassion</a></h1>
+		   </div>
+		   <!--div class="header-icons">
+			 <ul>
+				<li><a href="#" class="fb"> </a></li>
+				<li><a href="#" class="twit"> </a></li>
+				<li><a href="#" class="gmail"> </a></li>
+				<li><a href="#" class="dri"> </a></li>
+			 </ul>
+		   </div-->
+			<div class="clearfix"> </div>
+		  </div>
+    </div>
+  </div>
+<div class="top-nav-main">
+<div class="top-nav">
+            	<span class="menu"> <img src="images/icon.png" alt=""></span>	
+				<ul class="res">
+					<li><a href="index.html"><i class="glyphicon glyphicon-home"> </i>Home</a></li>
+					<li><a class="active" href="about.html"><i class="glyphicon glyphicon-user"> </i>About</a></li>					
+					<li><a href="events.html"><i class="glyphicon glyphicon-list-alt"> </i>Events</a></li>
+					<!--li><a href="blog.html"><i class="glyphicon glyphicon-picture"> </i>Blog</a></li-->
+					<li><a href="contact.html"><i class="glyphicon glyphicon-envelope"> </i>Contact</a></li>
+				</ul>
+				<!-- script-for-menu -->
+							 <script>
+							   $( "span.menu" ).click(function() {
+								 $( "ul.res" ).slideToggle( 300, function() {
+								 // Animation complete.
+								  });
+								 });
+							</script>
+			<!-- /script-for-menu -->
+	  </div>
+  </div>		
+</div>	
+<!--banner end here-->
+<!--about start here-->
+<div class="about">
+	<div class="container">
+		<div class="about-main">
+			<div class="about-top">
+				<h2>About</h2>
+				<p> </p>
+			</div>
+			<div class="about-bottom-main">
+				<div class="about-bot">
+					<!--point 1-->
+			  		<div class="col-md-6 about-bot-left" >
+			  			<div class="col-md-4 advan-cup" >
+			  				<a href="#" rel="title" class="zoom-mask">
+							    <span class="glyphicon glyphicon-user hovicon effect-4 sub-b"  aria-hidden="true"> </span>
+						    </a>
+			  			</div>
+			  			 <div class="col-md-8 advan-text" >
+				  				<h4>Get Register</h4>
+				  				<p>Do you like to give your hand to the people who need a help? or else do you need any help from somebody else? To make a donation, join for the events as a volunteer or request for donations  you should get register to this website </p>
+			  			 </div>	
+			  		  <div class="clearfix"> </div>
+			  		</div>
+					<!--point 2-->
+			  		<div class="col-md-6 about-bot-right" id="des2">
+			  			<div class="col-md-4 advan-cup" >
+			  				<!--a href="#" rel="title" class="zoom-mask"--> 
+							       <span class="glyphicon glyphicon-gift hovicon effect-4 sub-b" aria-hidden="true"> </span>
+						       <!--/a-->
+			  			</div>
+			  			 <div class="col-md-8 advan-text" >
+				  				<h4>Place Your Donation</h4>
+				  				<p>If you are willing to help people, you can add a donation through this website. You can select any donation type and payment type. After adding your donation you can withdraw money to the hsbc account of Exploration club. Account no:0192765378222   </p>
+			  			 </div>	
+			  		  <div class="clearfix"> </div>
+			  		</div>
+			  		<div class="clearfix"> </div>
+			  	</div>
+			  	<div class="about-bot">
+					<!--point 3-->
+			  		<div class="col-md-6 about-bot-left" id="des3">
+			  			<div class="col-md-4 advan-cup">
+			  				<!--a href="#" rel="title" class="zoom-mask"-->
+			  					 <span class="glyphicon glyphicon-list hovicon effect-4 sub-b" aria-hidden="true"> </span>							       
+						       <!--/a-->
+			  			</div>
+			  			 <div class="col-md-8 advan-text">
+				  				<h4>View Upcomming Events</h4>
+				  				<p>You can see the upcomming events of Exploration club so that you can join  </p>
+			  			 </div>	
+			  		  <div class="clearfix"> </div>
+			  		</div>
+					<!--point 4-->
+			  		<div class="col-md-6 about-bot-right" id="des4">
+			  			<div class="col-md-4 advan-cup">
+			  				<!--a href="#" rel="title" class="zoom-mask"-->
+							       <span class="glyphicon glyphicon-heart-empty hovicon effect-4 sub-b" aria-hidden="true"> </span>
+						       <!--/a-->
+			  			</div>
+			  			 <div class="col-md-8 advan-text">
+				  				<h4>Join as a Volunteer</h4>
+				  				<p>You can join the events which are organized by the Exploration club. If you are interested to join any event, just click on <i>interested</i>. After that you will get notifications about that event. </p>
+			  			 </div>	
+			  		  <div class="clearfix"> </div>
+			  		</div>
+			  		<div class="clearfix"> </div>
+			  	</div>
+				<br><br>
+				<div class="about-bot">
+					<!--point 5-->
+			  		<div class="col-md-12 about-bot-left" id="des3">
+			  			<div class="col-md-5 advan-cup">
+			  				<!--a href="#" rel="title" class="zoom-mask"-->
+			  					 <span class="glyphicon glyphicon-piggy-bank hovicon effect-4 sub-b" aria-hidden="true"> </span>							       
+						       <!--/a-->
+			  			</div>
+			  			 <div class="col-md-6 advan-text">
+				  				<h4>Request for  Donation</h4>
+				  				<p>If you or your charity organization need any donation, you are able  <br>to request for a donatoin. You will be able get a quick help through our website. </p>
+			  			 </div>	
+			  		  <div class="clearfix"> </div>
+			  		</div>
+					
+			  		
+			  	</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--about end here-->
+<!--testimonial start here-->
+<div class="testimonial">
+	<div class="container">
+		<div class="testimonial-main">
+			<h3>You don't need a reason to help people</h3>
+			<p></p>
+		</div>
+	</div>
+</div>
+<!--testimonial end here-->
+<!--team start here-->
+<div class="team">
+	<div class="container">
+		<div class="team-main">
+			<div class="team-top" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInDown;">
+				<h3>Volunteers</h3>
+				
+			</div>
+			<div class="team-bottom" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInRight;">
+			  <div class="col-md-3 team-grids">
+			    <!-- normal -->
+			    <div class="ih-item circle effect5"><a href="#">
+			        <div class="img"><img src="images/t1.jpg" alt="img" class="img-responsive"></div>
+			        <div class="info">
+			          <div class="info-back">
+			            <h3>Individually, we are<br>one drop.<br>Together, we are<br>an ocean</h3>		          
+			          </div>			          
+			        </div></a></div>
+			        <div class="team-bottom">
+			        	  <p>I became a volunteer for enhancing the education of poor children by conducting seminars.  </p>
+			        	  <ul>
+			        	  	<li><a href="#" class="fa"> </a></li>
+			        	  	<li><a href="#" class="tw"> </a></li>
+			        	  	<li><a href="#" class="g"> </a></li>
+			        	  </ul>
+			        </div>
+			        
+			    <!-- end normal -->
+			   </div>
+			  <div class="col-md-3 team-grids">
+			   <!-- normal -->
+			    <div class="ih-item circle effect5"><a href="#">
+			        <div class="img"><img src="images/t2.jpg" alt="img" class="img-responsive"></div>
+			        <div class="info">
+			          <div class="info-back">
+			            <h3>Individually, we are<br>one drop.<br>Together, we are<br>an ocean</h3>		           	            
+			          </div>
+			        </div></a></div>
+			        <div class="team-bottom">
+			        	  <p>I became a volunteer to help the people by engaging with helath programs  </p>
+			        	  <ul>
+			        	  	<li><a href="#" class="fa"> </a></li>
+			        	  	<li><a href="#" class="tw"> </a></li>
+			        	  	<li><a href="#" class="g"> </a></li>
+			        	  </ul>
+			        </div>
+			    <!-- end normal -->		 
+			  </div>
+			  <div class="col-md-3 team-grids">
+			    <!-- normal -->
+			    <div class="ih-item circle effect5"><a href="#">
+			        <div class="img"><img src="images/t3.jpg" alt="img" class="img-responsive"></div>
+			        <div class="info">
+			          <div class="info-back">
+			            <h3>Individually, we are<br>one drop.<br>Together, we are<br>an ocean</h3>			       
+			          </div>
+			        </div></a></div>
+			        <div class="team-bottom">
+			        	  <p>I became a volunteer to contribute for the social services like building temples etc.</p>
+			        	  <ul>
+			        	  	<li><a href="#" class="fa"> </a></li>
+			        	  	<li><a href="#" class="tw"> </a></li>
+			        	  	<li><a href="#" class="g"> </a></li>
+			        	  </ul>
+			        </div>
+			    <!-- end normal -->
+			  </div>
+			   <div class="col-md-3 team-grids">
+			    <!-- normal -->
+			    <div class="ih-item circle effect5"><a href="#">
+			        <div class="img"><img src="images/t4.jpg" alt="img" class="img-responsive"></div>
+			        <div class="info">
+			          <div class="info-back">
+			            <h3>Individually, we are<br>one drop.<br>Together, we are<br>an ocean</h3>		           
+			          </div>
+			        </div></a></div>
+			        <div class="team-bottom">
+			        	  <p>I love to work as a volunteer by engaging in emergancy services.</p>
+			        	  <ul>
+			        	  	<li><a href="#" class="fa"> </a></li>
+			        	  	<li><a href="#" class="tw"> </a></li>
+			        	  	<li><a href="#" class="g"> </a></li>
+			        	  </ul>
+			        </div>
+			    <!-- end normal -->			 
+			  </div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--team end here-->
+<!--footer start here-->
+<div class="footer">
+	<div class="container">
+		<div class="footer-main">
+			<div class="col-md-4 ftr-grid">
+				<h3>Navigation</h3>
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="events.html">Events</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+			<!--div class="col-md-4 ftr-grid">
+				<h3>Latest Tweet</h3>
+				<div class="tweets">
+					<div class="tweet-icon">
+						<span class="tweet-img"> </span>
+					</div>
+					<div class="tweet-text">
+						<p>Lorem ipsum</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur But I must explain to you how all this mistaken.</p>
+			</div-->
+			<div class="col-md-4 ftr-grid">
+				<h3>Keep In Touch</h3>
+				<div class="ftr-icons">
+			    	  	 <div class="ftr-iblock">
+			        	   <span class="glyphicon glyphicon-map-marker">  </span>
+			        	 </div>
+			        	 <div class="ftr-text">
+			        	 	<p>UCSC exploration club</p>
+			        	 </div>
+			        	 <div class="clearfix"> </div>
+			   </div>
+			   <div class="ftr-icons">
+			    	  	 <div class="ftr-iblock">
+			        	   <span class="glyphicon glyphicon-earphone">  </span>
+			        	 </div>
+			        	 <div class="ftr-text">
+			        	 	<p>071 755 5765</p>
+			        	 </div>
+			        	 <div class="clearfix"> </div>
+			   </div>
+			   <div class="ftr-icons">
+			    	  	 <div class="ftr-iblock">
+			        	   <span class="glyphicon glyphicon-envelope">  </span>
+			        	 </div>
+			        	 <div class="ftr-text">
+			        	 	<p><a href="mailto:info@example.com">hirukirana.e.s.f@gmail.com</a></p>
+			        	 </div>
+			        	 <div class="clearfix"> </div>
+			   </div>
+			</div>
+			<div class="clearfix"> </div>		
+		</div>
+	</div>
+</div>
+<!--footer end here-->
+<!--copy rights start here-->
+<div class="copy-right">
+	<div class="container">
+		 <div class="copy-rights-main">
+    	    <p>© 2016 Donate.  </p>
+    	 </div>
+    </div>
+</div>
+<!--copy right end here-->
+</body>
+</html>
